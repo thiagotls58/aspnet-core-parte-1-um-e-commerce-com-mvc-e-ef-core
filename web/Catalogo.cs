5197579f0661c393414343f86a504b2ae38a72e0
Thiago Luz <thiagotls58@gmail.com>
2021-01-11 08:29:24 -0300

    Aula 01

---
+++ web/Catalogo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web
+{
+    public class Catalogo : ICatalogo
+    {
+        public List<Livro> GetLivros()
+        {
+            var livros = new List<Livro>();
+            livros.Add(new Livro("001", "Anjos de demonios", 29.99m));
+            livros.Add(new Livro("002", "O codigo da vinci", 29.99m));
+            livros.Add(new Livro("003", "O simbolo perdido", 29.99m));
+
+            return livros;
+        }
+    }
+}
--- web/Startup.cs
+++ web/Startup.cs
@@ -16,24 +16,22 @@ namespace web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICatalogo, Catalogo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            ICatalogo catalogo = serviceProvider.GetService<ICatalogo>();
+            IRelatorio relatorio = new Relatorio(catalogo);
+            app.Run(async (context) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Olá Mundo!");
-                });
+                await relatorio.Imprimir(context);
             });
         }
     }
